f00e699bee8c54ea15badabcec845a4977aa67c7
buihuy1203 <buihuy1203@gmail.com>
2023-12-13 15:10:37 +0700

    Update Week 12

--- svmKernels.py
+++ svmKernels.py
@@ -6,7 +6,7 @@ Author: Eric Eaton, 2014
 """
 
 import numpy as np
-
+import math
 
 _polyDegree = 2
 _gaussSigma = 1
@@ -20,7 +20,8 @@ def myPolynomialKernel(X1, X2):
         Returns:
             An n1-by-n2 numpy array representing the Kernel (Gram) matrix
     '''
-    return #TODO
+    result = ((X1.dot(X2.T)) + 1) ** _polyDegree
+    return result
 
 
 
@@ -32,7 +33,19 @@ def myGaussianKernel(X1, X2):
         Returns:
             An n1-by-n2 numpy array representing the Kernel (Gram) matrix
     '''
-    return #TODO
+    result = -(pairwise(X1, X2))/(2 * (_gaussSigma ** 2))
+    return np.exp(result)
+def pairwise(X1, X2):
+    n = len(X1)
+    d = len(X2)
+
+    result = np.zeros([n, d])
+    for i in range(0, n):
+
+        for j in range(0, d):
+            result[i, j] = np.linalg.norm(X1[i] - X2[j]) ** 2
+            
+    return result
 
 
 
